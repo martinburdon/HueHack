597945e79a13ccd64e18d34c7623315dc53daf89
Martin Burdon <martin@martinburdon.co.uk>
2016-11-27 22:22:18 +0000

    Getting rooms

--- src/api.js
+++ src/api.js
@@ -3,30 +3,53 @@ import axios from 'axios';
 export default class api {
   constructor(url) {
     this.url = 'https://www.meethue.com/api/';
+    this.bridge = null;
+    this.bridgeIp = null;
   }
 
   setup() {
-    this.getBridges()
-      .then(data => {
-        this.bridge = data[0];
-        this.createUser()
+    if (sessionStorage.getItem('username')) {
+      console.log(sessionStorage.getItem('username'));
+      this.getBridges()
         .then(data => {
-          console.log('User ID: ', data);
-        })
-        .catch(error => {
-          console.log('Error: ', error);
+          this.bridge = data[0];
+          this.bridgeIp = this.bridge.internalipaddress;
+          this.groups(sessionStorage.getItem('username'));
         });
-      });
+    } else {
+      this.getBridges()
+        .then(data => {
+          this.bridge = data[0];
+          this.bridgeIp = this.bridge.internalipaddress;
+          this.prompForBridgePress();
+        });
+    }
   }
 
   getBridges() {
-    // return new Promise((resolve, reject) => {
     return axios
       .get('https://www.meethue.com/api/nupnp')
       .then(res => {
         return res.data;
       });
-    // });
+  }
+
+  prompForBridgePress() {
+    const result = window.confirm('Press the button on your Hue Bridge then click OK');
+
+    if (result) {
+      this.createUser()
+      .then(data => {
+        // TODO: Store in localStorage "hue_username"
+        // K4wVpeukrCX76j-k1qe5ezRmMadPVafc2wzqWXTW
+        console.log('User ID: ', data);
+        sessionStorage.setItem('username', data.username);
+        this.groups(data.username);
+      })
+      .catch(error => {
+        console.log('Error: ', error);
+      });
+    }
   }
 
   createUser() {
@@ -45,5 +68,14 @@ export default class api {
           }
         });
       });
+  }
+
+  groups(username) {
+    axios
+      .get(`http://${this.bridgeIp}/${username}/groups`)
+      .then(data => {
+        console.log(data);
+      });
+      // .then(res => filter(res.data, { type: 'Room' }));
   }
 };
